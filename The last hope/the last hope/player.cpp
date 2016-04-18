06d84aa91da8c3ee38329ba5fd7267c3656164f8
SrPerso <carlos@jardiper.es>
2016-04-18 23:27:28 +0200

    adding .cpp that was missing
    adding .cpp that was missing

--- The last hope/the last hope/player.cpp
+++ The last hope/the last hope/player.cpp
@@ -1,3 +1,4 @@
+
 //- -------- system -----------------
 #include <iostream>
 #include <stdio.h>
@@ -14,51 +15,46 @@
 //------------------------
 
 Player::Player(const char* name, const char* description, room* position) :
-entity(name, description,PLAYER),position(position){
+entity(name, description, PLAYER), position(position){
 }
 
 Player::~Player(){}
 
 
-void Player::go(world*World,const dir tgo){
-	
-	if (tf == true){/// quitar
+void Player::go(world*World, const dir tgo){
 
-		
-		 for (uint i = 0; i < (_EXITSNUMBER_); i++){//size
 
-			if (World->Exit[i]->src == position){//negarlo y ponerle continye
-//manteniendo el nivel de anidaccion
-				if (World->Exit[i]->direction == tgo){
 
-					if (World->Exit[i]->open == true){
+	for (uint i = 0; i < (_EXITSNUMBER_); i++){//size
 
-						position = World->Exit[i]->dest;
-						//position->look();
-						printf("\t %s \n\n", World->Exit[i]->dest->SayMyName());
-						printf("\t %s \n\n", World->Exit[i]->dest->SayTheDescription());
-						tf = false;
+		if (World->Exit[i]->src != position){ continue; }
 
-						break;
+		else if (World->Exit[i]->direction != tgo){ continue; }
 
-					}//if open
+		if (World->Exit[i]->open == true){
 
-					else{
+			position = World->Exit[i]->dest;
 
-						printf("hum... this door is close maybe you can open it with a key.. \n");
-						
-						tf = false;
-						break;
+			position->look();
 
-					}//else close
+			noStop = false;
 
-				}//if direccioner
+			break;
 
-			}//if look the position
+		}//if open
 
-		}//for looking every valor
+		else{
 
-	}// const
+			printf("hum... this door is close maybe you can open it with a key.. \n");
+
+			noStop = false;
+
+			break;
+
+		}//else close
+
+	}//for looking every valor
+
 
 }//void GOOO ------------------------------------------------------------
 
@@ -66,31 +62,32 @@ void Player::look()const{
 
 	printf("\t %s \n\n %s", position->SayMyName(), position->SayTheDescription());
 
-	if (position->entity_.size() >= 1)
+	if (position->Entity.size() >= 1)
 	{
-		printf("You can see a %s", position->entity_[0]->SayMyName());
-		for (uint i = 1; position->entity_.size() > i; i++)
+		printf("You can see a %s", position->Entity[0]->SayMyName());
+		for (uint i = 1; position->Entity.size() > i; i++)
 		{
-			printf(" and a %s,", position->entity_[i]->SayMyName());
+			position->look();
 		}
 		printf(".\n");
 	}
 
 }//look room ------------------------------------------------------------
-void Player::look(world* World,dir look)const{
+void Player::look(world* World, dir look)const{
 
 
 	for (int i = 0; i < _EXITSNUMBER_; i++){
 
-		if (World->Exit[i]->src == position){
 
-			if (World->Exit[i]->direction == look){
+		if (World->Exit[i]->src != position){ continue; }//position player
 
-				printf("%s\n", World->Exit[i]->description);
+		else if (World->Exit[i]->direction == look){
 
-			}//if direction to look
+			printf("%s\n", World->Exit[i]->name);
+			printf("%s\n", World->Exit[i]->description);
 
-		}//position player
+		}//if direction to look
+
 
 	}//for looking every valor
 
@@ -101,61 +98,57 @@ void Player::close(world* World, dir close){
 
 	for (int i = 0; i < _EXITSNUMBER_; i++){
 
-		if (World->Exit[i]->src == position){
+		if (World->Exit[i]->src != position){ continue; }
 
-			if (World->Exit[i]->direction == close){
+		else if (World->Exit[i]->direction != close){ continue; }
 
-				if (World->Exit[i]->open == true){
+		else if (World->Exit[i]->open == true){
 
-					World->Exit[i]->open = false;
+			World->Exit[i]->open = false;
 
-					closedoorFX();//FX closing door
+			closedoorFX();//FX closing door
 
-					printf("\n\n\t Maybe you close the door too hard...\n\n", World->Exit[i]->dest);
+			printf("\n\n\t Maybe you close the door too hard...\n\n", World->Exit[i]->dest);
 
-				}// close the door
+		}// close the door
 
-				else{
+		else{
 
-					printf("You can't do that.\n");
+			printf("You can't do that.\n");
 
-				}//else you cant open the door
-
-			}//direction to close
-
-		}//position of the player
+		}//else you cant open the door
 
 	}//for looking every valor
 
 }// close doors---------------------------------------------------------
 
-void Player::open(world* World,dir open){
+void Player::open(world* World, dir open){
 
 	for (int i = 0; i < _EXITSNUMBER_; i++){
 
-		if (World->Exit[i]->src == position){
+		if (World->Exit[i]->src != position){ continue; }//position of the player
 
-			if (World->Exit[i]->direction == open){
+		else if (World->Exit[i]->direction == open){ continue; }//direction to open
 
-				if (World->Exit[i]->open == false){
+		else if (World->Exit[i]->open == false){
 
-					World->Exit[i]->open = true;
+			World->Exit[i]->open = true;
 
-					opendoorFX();//FX closing door
+			opendoorFX();//FX closing door
 
-					printf("\t iii.... the door was open..\n", World->Exit[i]->dest);
+			printf("\t iii.... the door was open..\n", World->Exit[i]->dest);
 
-				}// close the door
+		}// close the door
 
-				else{
+		else{
 
-					printf("You can't do that.\n");
+			printf("You can't do that.\n");
 
-				}//else you cant open the door
+		}//else you cant open the door
 
-			}//direction to open
 
-		}//position of the player
+
+
 
 	}//for looking every valor
 
@@ -164,26 +157,26 @@ void Player::open(world* World,dir open){
 
 void Player::Inventory()const
 {
-	if (entity_.size() == 0){
+	if (PlayerItems.size() == 0){
 		printf("You haven't any items ry now\n");
 	}
-	for (unsigned int i = 0; entity_.size() > i; i++){
+	for (unsigned int i = 0; PlayerItems.size() > i; i++){
 
-		printf("%s\n", this->entity_[i]->SayMyName());
+		printf("%s\n", this->PlayerItems[i]->SayMyName());
 	}
 }//inventoryy ----------------------------------------------------
 
 void Player::pick(const mString item)
 {
-	if (entity_.size() < 1){
-		for (uint i = 0; position->entity_.size() > i; i++){
+	if (PlayerItems.size() < 1){
+		for (uint i = 0; position->RoomItems.size() > i; i++){
 
-			if (item == position->entity_[i]->SayMyName()){
+			if (item == position->RoomItems[i]->SayMyName()){
 
-				entity_.push_back(position->entity_[i]);
+				PlayerItems.push_back(position->RoomItems[i]);
 
-				printf("You picked up a %s", position->entity_[i]->SayMyName());
-				position->entity_.Remove(i);//delete the object in the position
+				printf("You picked up a %s", position->RoomItems[i]->SayMyName());
+				position->RoomItems.Remove(i);//delete the object in the position
 
 				break;
 			}
@@ -198,16 +191,16 @@ void Player::pick(const mString item)
 
 void Player::drop(const mString item)
 {
-	if (entity_.size() > 0){
-		for (uint i = 0; entity_.size() > i; i++)
+	if (PlayerItems.size() > 0){
+		for (uint i = 0; PlayerItems.size() > i; i++)
 		{
-			if (item == entity_[i]->SayMyName()){
+			if (item == PlayerItems[i]->SayMyName()){
 
-				position->entity_.push_back(entity_[i]);
+				position->RoomItems.push_back(PlayerItems[i]);
 
-				printf("You droped the %s", entity_[i]->SayMyName());
+				printf("You droped the %s", PlayerItems[i]->SayMyName());
 
-				entity_.Remove(i);//delete the object in the position
+				PlayerItems.Remove(i);//delete the object in the position
 
 				break;
 			}
@@ -220,32 +213,21 @@ void Player::drop(const mString item)
 }//DROOOPP THE BASSS .----------------------
 
 
-void Player::get(world* World,const mString item){
+void Player::get(world* World, const mString item, const mString box){
 
-	if (position == World->Room[8] || position == World->Room[0]){
+	if (position == World->Room[0]){
 
-		if (entity_.size() < _INVENTORY_SPACE_){
+		if (PlayerItems.size() < _INVENTORY_SPACE_){
 
-			for (unsigned int i = 0; World->item[7]->entity_.size() > i; i++)
-			{
-				if (item == World->item[2]->entity_[i]->SayMyName() && World->item[2]->entity_[i]->type == ITEM){
+			for (unsigned int i = 0; World->item[8]->BOXItems.size() > i; i++){
 
-					printf("You got a targed from the monkey shit");
-		
-					entity_.push_back(World->item[2]->entity_[i]);//transfer
+				if (item == World->item[8]->BOXItems[i]->SayMyName() && World->item[8]->BOXItems[i]->type == ITEM){
 
-					World->item[2]->entity_.Remove(i);//delete
+					printf("You got a %s from the %s", World->item[8]->BOXItems[i]->SayMyName(), box);
 
-					break;
+					PlayerItems.push_back(World->item[8]->BOXItems[i]);//transfer
 
-				}
-				else if (item == World->item[7]->entity_[i]->SayMyName() && World->item[7]->entity_[i]->type == ITEM){
-
-					printf("You got a cable from the toolbox");
-
-					entity_.push_back(World->item[7]->entity_[i]);//transfer
-					
-					World->item[7]->entity_.Remove(i);//delete
+					World->item[8]->BOXItems.Remove(i);//delete
 
 					break;
 
@@ -253,10 +235,112 @@ void Player::get(world* World,const mString item){
 			}
 			printf("Maybe you are wrong, there is nothing here");
 		}
-		
+
 		else{
 			printf("You have the bagg full");
 		}
 	}
 
 }//get ------------------------------------------------------
+
+void Player::put(world* World, const mString item, const mString box)
+{
+
+	if (PlayerItems.size() > 0){
+
+		for (unsigned int i = 0; PlayerItems.size() > i; i++)
+		{
+			if (item == PlayerItems[i]->SayMyName()){
+
+				World->item[8]->BOXItems.push_back(PlayerItems[i]);
+
+				printf("You put the %s to the %s", PlayerItems[i]->SayMyName(), box);
+
+				PlayerItems.Remove(i);
+
+
+				break;
+			}
+		}
+		printf("you havent this item ");
+
+	}
+
+	else{
+		printf("You can't put nothing there because YOU HAVENT ANY ITEM !");
+	}
+}//puttttttttt
+
+
+void Player::stats()
+{
+	system("cls");
+	printf("________________________________________________________________________________\n");
+	printf("\t ||HP = %i||\t\t", hp);
+	printf("||ATTACK = %i||\t\t", attack);
+	printf("||ATTACK SPEED = %i||", attackspeed);
+	printf("\n________________________________________________________________________________\n");
+
+}
+
+
+
+void Player::equip(world* World, const mString item2){
+
+	if (PlayerItems.size() >= 0){
+
+		for (uint i = 0; i < _ITEMNUMBER_; i++){
+
+			if (item2 == PlayerItems[i]->SayMyName() && ((item2 == "Stick") || (item2 == "Blazzer") || (item2 == "Stone") || (item2 == "Laser_Sword"))){
+
+				PlayerEquip.push_back(PlayerItems[i]);
+
+				attack += PlayerItems[i]->attack;
+				attackspeed += PlayerItems[i]->attackspeed;
+
+				printf("You equipped %s \n", PlayerItems[i]->name.C_str());
+				PlayerItems.Remove(i);
+				return;
+
+			}
+
+		}//for
+	}//if
+
+	printf("This object that you want to equip isnt in your inventory\n");
+}//EQUIPP
+
+void Player::unequip(world* World, const mString item2){
+
+	if (PlayerItems.size() >= 0){
+
+		for (uint i = 0; i < _ITEMNUMBER_; i++){
+
+			if (item2 == PlayerItems[i]->SayMyName() && ((item2 == "Stick") || (item2 == "Blazzer") || (item2 == "Stone") || (item2 == "Laser_Sword"))){
+
+
+				attack -= PlayerItems[i]->attack;
+				attackspeed -= PlayerItems[i]->attackspeed;
+
+				PlayerEquip.push_back(PlayerItems[i]);
+				printf("You unequip the %s.", PlayerItems[i]->SayMyName());
+				PlayerEquip.Remove(i);
+				return;
+			}
+
+			else{
+
+				printf("You haven't this item equipped\n");
+				return;
+
+			}//else
+
+
+
+		}//for
+	}//if
+	else{
+
+		printf("you havent any item! \n");
+	}
+}//UNEQUIPP
